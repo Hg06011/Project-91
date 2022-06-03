a11afb1dfd3446f53992294de14ceb360aec295e
Hg06011 <97067404+Hg06011@users.noreply.github.com>
2022-06-03 18:44:38 -0400

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -20,7 +20,7 @@ number2 = document.getElementById("number2").value;
 
 actual_answer = parseInt(number1) * parseInt(number2);
 
-question_number = "<h4>" + number1 +"X" + number2 + "</h4>";
+question_number = "<h4>" + number1 + " X " + number2 + "</h4>";
 input_box = "<br>answer  ; <input type= 'text' id ='input_check -box'>";
 check_button ="<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
 row = question_number + input_box + check_button
